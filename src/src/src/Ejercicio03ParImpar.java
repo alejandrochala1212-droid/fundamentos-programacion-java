5f218624e9709d2c4b01022cfb857ab1a8a70e7d
alejandrochala1212-droid <alejandrochala1212@gmail.com>
2026-05-10 20:25:39 -0500

    Create Ejercicio03ParImpar.java

---
+++ src/src/src/Ejercicio03ParImpar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio03ParImpar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa un número entero: ");
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número es par.");
+        } else {
+            System.out.println("El número es impar.");
+        }
+
+        sc.close();
+    }
+}
